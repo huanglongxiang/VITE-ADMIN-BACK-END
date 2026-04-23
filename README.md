00c1727b7ef985fc7bafac5d28c246082cbfd87b
LIU_COMRADE <848553347@qq.com>
2026-04-23 08:56:16 +0800

    Dockerfile and mk

--- README.md
+++ README.md
@@ -3,8 +3,16 @@ vite-admin后端系统
 - 基于go语言开发，使用go-zero框架、gorm作为数据库连接、gorm-gen作为代码生成、redis、swagger作为接口文档生成
 ``
 - deploy 项目部署相关信息，如部署时一些程序的配置，sql后者dockerfile
-- pkg 项目的公共目录
+- pkg 项目的公共目录****
 - docker-compose.yml docker compose 文件
 - Makefile 项目编译脚本程序
-
+  #先将程序编译成C进制可执行的文
+  GOOS=linux GoARCH=amd64 CGO_ENABLED=0 go build -o bin/user-rpc ./apps/user/rpc/user.go
+  #然后根据二进制文件构建成镜像文件
+  docker build -t user-rpc -f ./dockerfile_rpc .
+  再修改构建的镜像标签#
+  $ docker tag user-rpc registry.cn-hangzhou.aliyuncs.com/easy-im/user-rpc-test:latest
+  #然后推送到阿里云上
+  docker push registry.cn-hangzhou.aliyuncs.com/easy-im/user-rpc-test:1atest
+  #在部署的时候拉取下来构建容器运行即可
 ``
---
+++ deploy/mk/admin_rpc.mk
@@ -0,0 +1,28 @@
+VERSION=latest
+SERVER_NAME=core
+SERVER_TYPE=rpc
+#测试环境配置
+#docker的镜像发布地址
+DOCKER_REPO_TEST=registry.cn-hangzhou.aliyuncs.com/easy-im/${SERVER_NAME}-$SERVER_TYPE}-test
+#测试版本
+VERSION_TEST=$(VERSION)
+# 编译的程序名称
+APP_NAME_TEST=easy-im-${SERVER_NAME}-${SERVER_TYPE}-test
+#测试下的编译文件
+DOCKER_FILE_TEST=./deploy/dockerfile/Dockerfile_${SERVER_NAME}_${SERVER_TYPE}_test
+
+#测试环境的编译发布
+build-test:
+GOOS=linux GOARCH=amd64 CG0_ENABLED=0 go build -o bin/${SERVER_NAME}-${SERVER_TYPE}
+   ./apps/${SERVER_NAME}/${SERVER_TYPE}/${SERVER_NAME}.go
+   docker build . -f ${DOCKER_FILE_TEST} --no-cache -t ${APP_NAME_TEST}
+
+ #镜像的测试标签
+ tag-test:
+ @echo 'create tag ${VERSION_TEST}'
+ docker tag ${APP_NAME_TEST} ${DOCKER_REPO_TEST}:${VERSION_TEST}
+ publish-test:
+ @echo 'publish ${VERSION_TEST} to ${DOCKER_REPO_TEST}'
+ docker pUsh $(DOCKER_REPO_TEST):${VERSION_TEST}
+
+ release-test:build-test tag-test publish-test
